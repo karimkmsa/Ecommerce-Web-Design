70d335734b1f5137c37ae27825dc2ed27181950d
karimkmsa <khaledkarim808@gmail.com>
2026-05-16 15:11:27 +0300

    week1 frontend pages added

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,54 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { connection } from './dataBase/connection.js';
 
+const app = express();
+const port = 3000;
 
-
-
-
-
-const app = express()
-const port = 3000
 connection();
-
-
 
 app.use(express.json());
 
-app.get('', (req, res) => {
 
-    res.send("Hello World!");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+
+app.set("view engine", "ejs");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// Home Page
+app.get("/", (req, res) => {
+
+    res.render("Home");
 
 });
+
+
+// Products Page
+app.get("/products", (req, res) => {
+
+    res.render("ProductListingPage");
+
+});
+
+
+// Product Details Page
+app.get("/products/:id", (req, res) => {
+
+    const productId = req.params.id;
+
+    res.render("ProductDetailsPage", {
+        id: productId
+    });
+
+});
+
 app.listen(port, () => {
 
     console.log(`server is running....... on ${port}`);
