0281a46771fef4f602a84d0ef7bb7a65f858f4b0
karimkmsa <khaledkarim808@gmail.com>
2026-05-17 21:29:35 +0300

    user crud operation

--- modules/product/product.controller.js
+++ modules/product/product.controller.js
@@ -4,7 +4,7 @@ import productModel from "../../dataBase/models/product.model.js"
 
 export const addProduct = async(req,res)=>{
 let {name,price,category,description,stock} = req.body
-addedProduct = await productModel.insertMany({name,price,category,description,stock})
+const addedProduct = await productModel.insertMany({name,price,category,description,stock})
 
 
 res.json({messsege:"Products", addedProduct})
@@ -15,7 +15,7 @@ res.json({messsege:"Products", addedProduct})
 
 export const updateProduct = async(req,res)=>{
 let {_id,name,price,category,description,stock} = req.body
-updatedProduct = await productModel.findByIdAndUpdate(_id,{name,price,category,description,stock},{new:true})
+const updatedProduct = await productModel.findByIdAndUpdate(_id,{name,price,category,description,stock},{new:true})
 
 
 res.json({messsege:"Products", updatedProduct})
@@ -27,8 +27,8 @@ res.json({messsege:"Products", updatedProduct})
 
 
 export const deleteProduct = async(req,res)=>{
-let {_idid} = req.body
-deletedProduct = await productModel.deleteOne(_id)
+let {_id} = req.body
+const deletedProduct = await productModel.deleteOne(_id)
 
 
 res.json({messsege:"Products", deletedProduct})
@@ -38,7 +38,7 @@ res.json({messsege:"Products", deletedProduct})
 }
 
 export const getProduct = async(req,res)=>{
-allproducts = await productModel.find()
+const allproducts = await productModel.find()
 
 
 res.json({messsege:"Products", allproducts})
--- modules/product/product.routes.js
+++ modules/product/product.routes.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import { addProduct, deleteProduct, getProduct, updateProduct } from './product.controller.js'
 
 export default router = express.Router()
 
 
 
 
-router.get("/",)
+router.get("/",getProduct)
+router.post("/",addProduct)
+router.put("/updateProduct",updateProduct)
+router.delete("/deleteProduct",deleteProduct)
